7e143ec682af8fee63a58329348769228a770f9d
irina8660 <ira.efremova95@gmail.com>
2025-03-07 18:47:34 +0200

    add modal

--- src/main.js
+++ src/main.js
@@ -3,3 +3,14 @@ import 'swiper/css';
 
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
+
+import './js/header.js';
+import './js/hero.js';
+import './js/about-me.js';
+import './js/benefits.js';
+import './js/projects.js';
+import './js/faq.js';
+import './js/covers.js';
+import './js/reviews.js';
+import './js/work-together.js';
+import './js/modal.js';
