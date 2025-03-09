d0f24b1291b158b98b0e0b8ab68854063d2c23ac
irina8660 <ira.efremova95@gmail.com>
2025-03-10 00:39:26 +0200

    about me

---
+++ src/js/about-me.js
@@ -0,0 +1,8 @@
+import Accordion from 'accordion-js';
+import 'accordion-js/dist/accordion.min.css';
+
+new Accordion('.accordion-container', {
+  duration: 250,
+  openOnInit: [0],
+  // collapse: false,
+});
--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
-import Swiper from 'swiper';
-import 'swiper/css';
-
-import Accordion from 'accordion-js';
-import 'accordion-js/dist/accordion.min.css';
+import './js/about-me.js';
+import './js/benefits.js';
+import './js/covers.js';
+import './js/faq.js';
+import './js/header.js';
+import './js/hero.js';
+import './js/projects.js';
+import './js/reviews.js';
+import './js/work-together.js';
