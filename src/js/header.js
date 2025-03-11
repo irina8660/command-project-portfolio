98424542e199a7e8ddfb064bda5e9fc99cbac377
Liia <1991safronova@gmail.com>
2025-03-11 23:27:29 +0100

    change in header

--- src/js/header.js
+++ src/js/header.js
@@ -18,3 +18,27 @@ orderButton.addEventListener('click', () => {
     .querySelector('.work-together')
     .scrollIntoView({ behavior: 'smooth' });
 });
+
+// Mobile menu
+
+document.addEventListener('DOMContentLoaded', () => {
+  const menuOpenBtn = document.querySelector('[data-menu-open]');
+  const menuCloseBtn = document.querySelector(
+    '.header__mobile-menu-close-button'
+  );
+  const mobileMenu = document.querySelector('.header__mobile-menu-container');
+  const mobileNavLinks = document.querySelectorAll('.header__mobile-nav-link');
+
+  const toggleMenu = () => {
+    const isOpen = mobileMenu.classList.contains('is-open');
+    mobileMenu.classList.toggle('is-open');
+    document.body.style.overflow = isOpen ? '' : 'hidden';
+  };
+
+  menuOpenBtn.addEventListener('click', toggleMenu);
+  menuCloseBtn.addEventListener('click', toggleMenu);
+
+  mobileNavLinks.forEach(link => {
+    link.addEventListener('click', toggleMenu);
+  });
+});
